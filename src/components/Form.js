fecf176fdd77737ecb7a653b3ffc444ee18d709a
Jenny Chau <jnechau@gmail.com>
2021-11-23 23:43:34 -0500

    commit tree 110632f15b34b6e2dbfe1ac6556f956bc7bf45b7

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,13 +14,49 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()//stop the page from refreshing
+    if (firstName.length > 0) {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      }; //end of formsData object- uses values from state (instead of the DOM) and makes it an object
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");//clearing input fields
+      setLastName("");//clearing input fields 
+      setErrors([])
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
+
+const listOfSubmissions = submittedData.map((data, index) => {
   return (
-    <form>
+    <div key={index}>
+      {data.firstName} {data.lastName}
+    </div>
+  );
+});
+
+return (
+  <>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
-  );
+    {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
+    <h3> Submissions </h3>
+    {listOfSubmissions}
+  </>
+);
 }
 
 export default Form;
